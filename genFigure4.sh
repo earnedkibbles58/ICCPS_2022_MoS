972ccbf0f1e34664b960e5f5c7b536af63a238ac
Matthew Cleaveland <mcleav@seas.upenn.edu>
2022-01-26 15:13:39 -0500

    more script changes

--- genFigure4.sh
+++ genFigure4.sh
@@ -4,9 +4,9 @@ pushd /ICCPS_2022_MoS-repeatabilityPackage/code/waterTank/experiments/diffWLDelt
 
 echo "Running code to generate figure 4. This should all take around 45 mins."
 
-echo "Running runWaterTankDiffDeltaWLs_baseline.py. This should take around 2 mins"
+echo "Running runWaterTankDiffDeltaWLs_baseline.py. This should take around 30 mins"
 python3 runWaterTankDiffDeltaWLs_baseline.py
-echo "Running runWaterTankDiffDeltaWLs_trimmedBaseline.py. This should take around 30 seconds"
+echo "Running runWaterTankDiffDeltaWLs_trimmedBaseline.py. This should take around 15 mins"
 python3 runWaterTankDiffDeltaWLs_trimmedBaseline.py
 echo "Saving figure to  /ICCPS_2022_MoS-repeatabilityPackage/results/waterTank/diffWLDeltas/plots/if13.5_of4.3_numSteps10/waterTankDiffWLsPlot.png"
 python3 visWaterTankDiffDeltaWLs.py
