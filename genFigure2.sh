d27adc42220799c2fe573acab74a3bc503b99371
Matthew Cleaveland <mcleav@seas.upenn.edu>
2022-01-26 13:19:41 -0500

    more cosmetic changes

--- genFigure2.sh
+++ genFigure2.sh
@@ -2,8 +2,12 @@
 
 pushd /ICCPS_2022_MoS-repeatabilityPackage/code/AEBS/experiments/diffDeltaDs
 
+echo "Running runAEBSDiffDeltaDs_baseline.py. This should take around 2 mins"
 python3 runAEBSDiffDeltaDs_baseline.py
+echo "Running runAEBSDiffDeltaDs_trimmedbaseline.py. This should take around 30 seconds"
 python3 runAEBSDiffDeltaDs_trimmedbaseline.py
+echo "Visualizing"
 python3 visAEBSDiffDeltaDsResults.py
+echo "Done"
 
 popd
