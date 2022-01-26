a8fdad87237f9cb6df9e7ae299cf08c3a8c744f3
Matthew Cleaveland <mcleav@seas.upenn.edu>
2022-01-26 15:30:49 -0500

    yet more script changes

--- genFigure6.sh
+++ genFigure6.sh
@@ -4,15 +4,15 @@ pushd /ICCPS_2022_MoS-repeatabilityPackage/code/waterTank/experiments/largeModel
 
 echo "Running code to generate figure 6. This should all take around 21 mins."
 
-echo "Running runAEBSScalabilitySMC_baseline.py. This should take around 9 mins."
-python3 runAEBSScalabilitySMC_baseline.py
-echo "Running runAEBSScalabilitySMC_trimmedbaseline.py. This should take around 9 mins."
-python3 runAEBSScalabilitySMC_trimmedbaseline.py
-echo "Running runAEBSScalabilitySMCFewSchSamples_trimmedbaseline.py. This should take around 3 mins."
-python3 runAEBSScalabilitySMCFewSchSamples_trimmedbaseline.py
+echo "Running runWaterTankScalabilitySMC_baseline.py. This should take around 9 mins."
+python3 runWaterTankScalabilitySMC_baseline.py
+echo "Running runWaterTankScalabilitySMC_trimmedbaseline.py. This should take around 9 mins."
+python3 runWaterTankScalabilitySMC_trimmedbaseline.py
+echo "Running runWaterTankScalabilitySMCFewSchSamples_trimmedbaseline.py. This should take around 3 mins."
+python3 runWaterTankScalabilitySMCFewSchSamples_trimmedbaseline.py
 echo "Saving figures to /ICCPS_2022_MoS-repeatabilityPackage/results/waterTank/largeModel_SMC/plots/if13.5_of4.3_deltawl5_numSteps10/runTimesPlot_withFew.png"
 echo "and /ICCPS_2022_MoS-repeatabilityPackage/results/waterTank/largeModel_SMC/plots/if13.5_of4.3_deltawl5_numSteps10/safetyProbsPlot_withFew.png"
-python3 visAEBSScalabilityResultsSMC.py
+python3 visWaterTankScalabilitySMC.py
 
 echo "Done"
 
